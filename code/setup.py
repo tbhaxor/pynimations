1d131aa8d0d7dbb11de581ad71b5308f91f8d1d1
tbhaxor <tbhaxor@gmail.com>
2017-12-10 03:31:14 +0530

    setup added

---
+++ code/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from webbrowser import open_new_tab
+
+setup(name="pynimations",
+      platforms="Cross Platform",
+      packages=["pynimations"],
+      version="0.1.1b1",
+      author_email="tbhaxor@gmail.com",
+      author="Gurkirat Singh",
+      url="https://tbhaxor.github.io/pynimations/",
+      download_url="",
+      keywords="pynimations gurkirat_singh tbhaxor",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+      license="GPL-3.0",
+      python_requires='>=3',
+
+      )
+open_new_tab("https://tbhaxor.github.io/pynimations/")
