190287b0abe92b657e6049a01a28b3c27f257a90
tbhaxor <tbhaxor@gmail.com>
2018-03-11 20:38:37 +0530

    added scroll in __init__

--- code/pynimations/__init__.py
+++ code/pynimations/__init__.py
@@ -2,3 +2,4 @@ import pynimations.progressbar as progressbar
 import pynimations.rotator as rotator
 import pynimations.casetoggle as casetoggle
 import pynimations.typewriter as typewriter
+import pynimations.scroll as scroll
